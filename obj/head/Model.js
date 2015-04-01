8ea9fa682d1d87fbad31b58047e7130568e97617
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2015-03-31 23:11:08 -0700

    first commit
    works in a simple manner

---
+++ obj/head/Model.js
@@ -0,0 +1,31 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "ModelNan.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 22903,
+        "faces"         : 20000,
+        "normals"       : 22903,
+        "uvs"           : 22903,
+        "materials"     : 1
+    },
+
+    "materials": [	{
+	"DbgColor" : 16777215,
+	"DbgIndex" : 0,
+	"DbgName" : "material0",
+	"colorAmbient" : [1.0, 1.0, 1.0],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 1,
+	"mapDiffuse" : "Model.jpg",
+	"specularCoef" : 0.0,
+	"transparency" : 1.0,
+	"transparent" : true
+	}],
+
+    "buffers": "Model.bin"
+
+}
